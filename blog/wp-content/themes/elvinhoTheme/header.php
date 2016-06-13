8d3f68bd94e283d4f927996fa26124d3851a4806
Elvio <elviobg@gmail.com>
2016-06-12 23:37:41 -0300

    [elvino.net][blog] Intial theme configuration

---
+++ blog/wp-content/themes/elvinhoTheme/header.php
@@ -0,0 +1,51 @@
+<html> 
+<head> 
+	<meta charset="utf-8">
+	<title>Elvinho</title> 
+
+  <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+  <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; <?php bloginfo('charset'); ?>" />
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" />
+  
+  <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+  <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+  <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+  
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link href="style.css" rel="stylesheet" type="text/css">
+  
+  <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/linecons.css" rel="stylesheet" type="text/css">
+	<link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+	<link href="css/responsive.css" rel="stylesheet" type="text/css">
+	<link href="css/animate.css" rel="stylesheet" type="text/css">
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,900,700,700italic,400italic,300italic,300,100italic,100,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Dosis:400,500,700,800,600,300,200' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" src="js/jquery.1.8.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.js"></script>
+	<script type="text/javascript" src="js/wow.js"></script>
+	<script type="text/javascript" src="js/classie.js"></script>
+  <?php wp_head(); ?> 
+</head> 
+<body>
+  <!--Header_section-->
+  <header id="header_outer">
+    <div class="container">
+      <div class="header_section">
+        <nav class="nav" id="nav">
+          <ul class="">
+            <li><a href="../../../../../elvinho.net/index.html#about">Home</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+        </nav>
+        <a class="res-nav_click animated wobble wow"  href="javascript:void(0)"><i class="fa-bars"></i></a> </div>
+    </div>
+  </header>
+  <!--Header_section--> 
